63963aaf5c1120127433833cf50a80babf00dd16
anastasia1002 <56667810+anastasia1002@users.noreply.github.com>
2020-05-26 23:16:26 +0300

    done

---
+++ lab10/lab10.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lab10._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose unit: 1-byte, 2-kB, 3-MB, 4-GB");
+            int unit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How much memory do you want to compare?");
+            double userMemory = Convert.ToDouble(Console.ReadLine());
+
+            RAM m1 = new RAM("Producer1", new DateTime(2015, 12, 3), 2, 100);
+
+            Console.WriteLine($"Convert Mb to Gb:{m1.ToGigabyte(m1.MemoryVolume)}"); 
+
+            Console.WriteLine($"comparison of memory objects :{m1.ComapreMemory(unit, userMemory)}"); 
+            Console.WriteLine($"number of years from production:{m1.Age()}");
+
+        }
+    }
+}
---
+++ lab10/lab10.2/RAM.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab10._2
+{
+    class RAM
+    {
+        private string producer;
+        private DateTime date;
+        private double memoryVolume;
+        private double price;
+
+        public string Producer { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public double MemoryVolume { get; set; }
+
+        public double Price { get; set; }
+
+        public RAM(string producer, DateTime date, double memoryVolume, double price)
+        {
+            Producer = producer;
+            Date = date;
+            MemoryVolume = memoryVolume;
+            Price = price;
+        }
+
+        public double ToByte(double memory)
+        {
+            return memory * Math.Pow(1024, 2);
+        }
+
+        public double ToKilobyte(double memory)
+        {
+            return memory * 1024;
+        }
+
+        public double ToGigabyte(double memory)
+        {
+            return memory * Math.Pow(1024, -1);
+        }
+
+        public const double Epsilon = 4.94065645841247E-324;
+        public bool ComapreMemory(int unit, double userMemory)
+        {
+            if (unit == 1) 
+                return Math.Abs(this.ToByte(MemoryVolume) - userMemory) < Epsilon;
+            if (unit == 2) 
+                return Math.Abs(this.ToKilobyte(MemoryVolume)) - userMemory < Epsilon;
+            if (unit == 3) 
+                return Math.Abs(MemoryVolume - userMemory) < Epsilon;
+            if (unit == 4)
+                return Math.Abs(this.ToGigabyte(MemoryVolume) - userMemory) < Epsilon;
+            return false;
+
+        }
+
+        public double Age() 
+        {
+            return DateTime.Now.Year - Date.Year;
+        }
+    }
+}
